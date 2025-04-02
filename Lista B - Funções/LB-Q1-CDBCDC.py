64af4945e7a32194223a8b5ef257ca30e2a77c1c
matheusydev <matheusylanaraujo@gmail.com>
2025-04-02 10:42:03 -0300

    atualização da tarefa

--- Lista B - Funções/LB-Q1-CDBCDC.py
+++ Lista B - Funções/LB-Q1-CDBCDC.py
@@ -1,17 +1,27 @@
 # Entrada
 
-    #CDB valor final = investimento *(1+ taxa de juros)^tempo
-
-investimentoCDB = float(input("insira o valor que sera investido no CDB: "))
-taxa = float(input("insira o valor taxa do banco: "))
-tempo = float(input("insira o tempo investido: "))
-
-    #CDC valor final = investimento * (taxa de jurus (1 + taxa de juros )^n ) // ((1 + taxa de juros)^n - 1)
-
-# Processamento 
-valorfinal = investimentoCDB * (1 + taxa) ** tempo 
+investimento = float(input("insira o valor que sera investido: "))
 
 
-#Saída 
+def calcularCDB():
+    taxaCDB = float(input("insira o valor da taxa do banco: "))
+    tempoCDB = int(input("insira o tempo que sera investido (anos): "))
+    jurosCompostoCDB = investimento * (1 + (taxaCDB / 100)) ** tempoCDB
+    valorCDB = jurosCompostoCDB - investimento
+    return valorCDB
 
-print(f"{valorfinal}")
+
+
+def calcularCDC():
+    taxaCDC = float(input("insira a taxa do CDC para cada mês: "))
+    parcelaCDC = int(input("insira o total de parcelas para calcular o CDC: "))
+    jurosCompostoCDC = investimento * (1 + (taxaCDC / 100)) ** parcelaCDC
+    valorCDC = jurosCompostoCDC - investimento
+    return valorCDC
+    
+ 
+#jurosComposto = calcularJurosComposto()    
+valorCDB = calcularCDB()
+print(f"CDB = {valorCDB:.2f}")
+valorCDC = calcularCDC()
+print(f"CDC  = {valorCDC:.2f}")
